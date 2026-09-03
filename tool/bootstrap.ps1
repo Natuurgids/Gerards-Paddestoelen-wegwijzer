517e99ada68b4763e07a17ac3551b90ca3ada4cd
Natuurgids.org <Snippit@chimaera-management.com>
2026-09-03 10:19:05 +0200

    Add Windows Flutter bootstrap script

---
+++ tool/bootstrap.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = 'Stop'
+
+if (-not (Get-Command flutter -ErrorAction SilentlyContinue)) {
+  throw 'Flutter is not available on PATH. Install Flutter first: https://docs.flutter.dev/get-started/install'
+}
+
+Write-Host 'Generating Flutter platform scaffolding...'
+flutter create . --platforms=android,ios --project-name gerards_paddestoelen_wegwijzer --org nl.natuurgids
+
+Write-Host 'Installing dependencies...'
+flutter pub get
+
+Write-Host 'Running analyzer...'
+flutter analyze
+
+Write-Host 'Bootstrap complete. Run: flutter run'
