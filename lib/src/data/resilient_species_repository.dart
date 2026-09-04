84587b6b77f7041fd2235e5c6e6400e290c2b1dc
Natuurgids.org <Snippit@chimaera-management.com>
2026-09-04 09:53:05 +0200

    Add asset-first species detail repository

---
+++ lib/src/data/resilient_species_repository.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'models.dart';
+import 'reference_asset_store.dart';
+import 'repositories.dart';
+
+class ResilientSpeciesRepository extends SpeciesRepository {
+  ResilientSpeciesRepository({DatabaseProvider? databaseProvider})
+      : _preferDatabase = databaseProvider != null,
+        super(databaseProvider: databaseProvider);
+
+  static const _databaseBudget = Duration(seconds: 2);
+
+  final bool _preferDatabase;
+
+  @override
+  Future<SpeciesDetail?> detail(int id, String languageCode) async {
+    if (!_preferDatabase) {
+      final asset = await ReferenceAssetStore.instance.speciesDetail(
+        id,
+        languageCode,
+      );
+      if (asset != null) return asset;
+    }
+
+    try {
+      return await super.detail(id, languageCode).timeout(_databaseBudget);
+    } on Object {
+      return ReferenceAssetStore.instance.speciesDetail(id, languageCode);
+    }
+  }
+}
