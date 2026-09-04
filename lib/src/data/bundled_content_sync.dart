2923a2e3d7e0e54bb614bc7be2818731642f05e4
Natuurgids.org <Snippit@chimaera-management.com>
2026-09-05 00:22:33 +0200

    Resync normalized conservation metadata

--- lib/src/data/bundled_content_sync.dart
+++ lib/src/data/bundled_content_sync.dart
@@ -6,7 +6,7 @@ class BundledContentSync {
   const BundledContentSync._();
 
   static const contentKey = 'reference-content';
-  static const revision = 6;
+  static const revision = 7;
 
   static Future<List<String>> runIfNeeded(
     Database db,
